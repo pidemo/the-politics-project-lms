336fb7c8520d6017b03481fc779c1e6bf7e39831
pidemo <81054973+pidemo@users.noreply.github.com>
2025-11-24 18:00:29 +0100

    Added Parent Wrapper Conditional Visibility
    Wrapper for backlinks is hidden by default, unless there are some backlinks, in which case we remove the .is-hidden-onload class to make wrapper visible.

--- backlinks.js
+++ backlinks.js
@@ -6,21 +6,18 @@ const schemesOfWorkBackLinksWrapper = document.querySelector(
 );
 
 const setBackLinks = (element, path) => {
+  // Get slugs and names elements
   const slugsElement = element.querySelector(".backlinks-slugs");
   const namesElement = element.querySelector(".backlinks-names");
   if (!slugsElement || !namesElement) return;
-  if (slugsElement.innerHTML.length === 0) {
-    element.remove();
-    return;
-  }
 
+  // Get slugs and names
   const slugs = slugsElement.innerHTML.split(",");
   const names = namesElement.innerHTML.split(",");
   const template = element.querySelector(".tag");
   if (!template || !slugs || !names) return;
 
-  console.log(slugs.length);
-
+  // Create backlinks
   slugs.forEach((slug, index) => {
     const backlink = template.cloneNode(true);
     backlink.classList.remove("is-hidden-onload");
@@ -29,6 +26,13 @@ const setBackLinks = (element, path) => {
     element.appendChild(backlink);
   });
 
+  // Make parent wrapper visible
+  const parentWrapper = element.parentElement;
+  if (parentWrapper) {
+    parentWrapper.classList.remove("is-hidden-onload");
+  }
+
+  // Remove template items
   slugsElement.remove();
   namesElement.remove();
   template.remove();
